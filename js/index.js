90492f019f0c29e31638f93d314b292f52097e2b
xdimizas <christos.dimizas@gmail.com>
2016-04-30 21:36:12 +0300

    Work experience js UI | In progress

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/**
+ * Created by christos on 30/4/2016.
+ */
+
+//$(document).ready(function() {
+//    $(document).scroll(function () {
+//        var scroll = $(this).scrollTop();
+//        var topDist = $("#nav").position();
+//        if (scroll > topDist.top) {
+//            $('nav').css({"position":"fixed","top":"0"});
+//        } else {
+//            $('nav').css({"position":"static","top":"auto"});
+//        }
+//    });
+//});
+
+// LOAD WORK EXPERIENCE VIEW
+$( document ).ready(function(){
+    $("#work-experience").load("cv-views/work_experience.html");
+});
+
+// Smooth scrolling when clicking an anchor link
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
+
