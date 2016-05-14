f84017f0909799aecd444ad5ada928a45e8a73d3
xdimizas <christos.dimizas@gmail.com>
2016-05-14 20:37:56 +0300

    Respective pages added to each sidebar link | TODO: Fix main contsiner CSS

--- christos-dimizas-cv-angular-version/app/js/app.js
+++ christos-dimizas-cv-angular-version/app/js/app.js
@@ -18,77 +18,83 @@ angular.module('dimizasCVApp', ['ui.router'])
             .state('app', {
                 url:'/',
                 views: {
-                    'header': {
+                    'sidebar': {
                         templateUrl : 'views/sidebar.html'
                     },
                     'landingPage': {
-                        templateUrl : 'views/landingPage.html'
-                    },
-                    'workExperience': {
-                        templateUrl : 'views/workExperience.html'
-                    },
-                    'education': {
-                        templateUrl : 'views/education.html'
-                    },
-                    'skills': {
-                        templateUrl : 'views/skills.html'
-                    },
-                    'interests': {
-                        templateUrl : 'views/interests.html'
-                    },
-                    'contact': {
-                        templateUrl : 'views/contact.html'
-                    },
-                    'footer': {
-                        templateUrl : 'views/footer.html'
+                        templateUrl : 'views/landingPage.html',
+                        controller  : 'IndexController'
                     }
+                    //'skills': {
+                    //    templateUrl : 'views/skills.html'
+                    //},
+                    //'interests': {
+                    //    templateUrl : 'views/interests.html'
+                    //},
+                    //'contact': {
+                    //    templateUrl : 'views/contact.html'
+                    //},
+                    //'footer': {
+                    //    templateUrl : 'views/footer.html'
+                    //}
                 }
 
-            });
+            })
 
-            //// route for the aboutus page
-            //.state('app.aboutus', {
-            //    url:'aboutus',
-            //    views: {
-            //        'content@': {
-            //            templateUrl : 'views/aboutus.html',
-            //            controller  : 'AboutController'
-            //        }
-            //    }
-            //})
-            //
-            //// route for the contactus page
-            //.state('app.contactus', {
-            //    url:'contactus',
-            //    views: {
-            //        'content@': {
-            //            templateUrl : 'views/contactus.html',
-            //            controller  : 'ContactController'
-            //        }
-            //    }
-            //})
-            //
-            //// route for the menu page
-            //.state('app.menu', {
-            //    url: 'menu',
-            //    views: {
-            //        'content@': {
-            //            templateUrl : 'views/menu.html',
-            //            controller  : 'MenuController'
-            //        }
-            //    }
-            //})
-            //
-            //// route for the dishdetail page
-            //.state('app.dishdetails', {
-            //    url: 'menu/:id',
-            //    views: {
-            //        'content@': {
-            //            templateUrl : 'views/dishdetail.html',
-            //            controller  : 'DishDetailController'
-            //        }
-            //    }
-            //});
+            // route for the Work & Academic Experience page
+            .state('app.workExperience', {
+                url:'work-academic-experience',
+                views: {
+                    'landingPage@': {
+                        templateUrl : 'views/workExperience.html'
+                        //controller  : 'WorksController'
+                    }
+                }
+            })
+
+            // route for the education & coursework page
+            .state('app.education', {
+                url:'education-coursework',
+                views: {
+                    'landingPage@': {
+                        templateUrl : 'views/education.html'
+                        //controller  : 'EducationController'
+                    }
+                }
+            })
+
+            // route for the technical skills page
+            .state('app.skills', {
+                url: 'technical-skills',
+                views: {
+                    'landingPage@': {
+                        templateUrl : 'views/skills.html'
+                        //controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the interests & achievements page
+            .state('app.interests', {
+                url: 'interests',
+                views: {
+                    'landingPage@': {
+                        templateUrl : 'views/interests.html'
+                        //controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the contact page
+            .state('app.contact', {
+                url: 'contact',
+                views: {
+                    'landingPage@': {
+                        templateUrl : 'views/contact.html',
+                        controller  : 'ContactController'
+                    }
+                }
+            });
 
         $urlRouterProvider.otherwise('/');
     })
